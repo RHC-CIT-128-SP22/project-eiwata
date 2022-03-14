3e2f8a154fbc45cf87cf0b9462ca83b017f2da41
eiwata <iwata.erika@gmail.com>
2022-03-14 14:47:58 -0700

    pygame window

--- main.py
+++ main.py
@@ -4,3 +4,30 @@
     CIT 128: Python Programming II
     Student Directed Project
 '''
+
+#modules
+import pygame
+
+#initialize the pygame
+pygame.init()
+
+#create the screen
+screen = pygame.display.set_mode((800, 600))
+
+#title and icon
+pygame.display.set_caption("The Call of Cthulhu - Survival Horror Game")
+icon = pygame.image.load('cthulhu.png')
+pygame.display.set_icon(icon)
+
+#Game Loop
+running = True
+while running:
+
+    #window stays open until user quit game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    #black rgb background
+    screen.fill((0, 0, 0))
+    pygame.display.update()
