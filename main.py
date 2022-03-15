19793b2819be51c81ebb633e6ba0c1e79e9f67d5
eiwata <iwata.erika@gmail.com>
2022-03-14 22:41:35 -0700

    start sound added

--- main.py
+++ main.py
@@ -5,31 +5,40 @@
     Student Directed Project
 '''
 
-#modules
+
 import pygame
 from pygame import mixer
+#import time
 
-#initialize the pygame
+#initialize game
 pygame.init()
 mixer.init()
 
-#create the screen and set volume
-screen = pygame.display.set_mode((800, 600))
-mixer.music.set_volume(0.8)
-
-#set title and icon (for window)
-pygame.display.set_caption("The Call of Cthulhu - Survival Horror Game")
+#load display and audio
 icon = pygame.image.load("pictures/icon.png")
-pygame.display.set_icon(icon)
-
-#load audio and pictures
 startBG = pygame.image.load("pictures/startBG.png")
 title = pygame.image.load("pictures/mainTitle.png")
 startButton = pygame.image.load("pictures/startButton.png")
 opening = mixer.music.load("audio/BeforeDawn.wav")
 
+#set display and audio
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("The Call of Cthulhu - Survival Horror Game")
+pygame.display.set_icon(icon)
+mixer.music.set_volume(0.7)
+
+#sound effects
+startSound = pygame.mixer.Sound("audio/wildBeastRoar.wav")
+
 #play background music
 mixer.music.play(-1)
+
+def game_intro():
+    #start game display
+    screen.blit(startBG, (0,70))
+    screen.blit(title, (40, 50))
+    screen.blit(startButton, (350, 470))
+     
 
 ###############  MAIN GAME LOOP  ################
 running = True
@@ -40,14 +49,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    #start game display
-    screen.blit(startBG, (0,70))
-    screen.blit(title, (40, 50))
-    screen.blit(startButton, (350, 470))
+    game_intro()
+
+    if event.type == pygame.MOUSEBUTTONUP:
+        pos = pygame.mouse.get_pos()
+        pygame.mixer.Sound.play(startSound)
 
     pygame.display.update()
 
 ################  EXIT GAME LOOP ################
+
 
 #stop background music
 mixer.music.stop()
