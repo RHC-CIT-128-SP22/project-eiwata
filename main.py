dff719d9a4de37c1fb5fd141b375818837d1ab97
eiwata <iwata.erika@gmail.com>
2022-03-14 19:33:50 -0700

    background music

--- main.py
+++ main.py
@@ -7,24 +7,31 @@
 
 #modules
 import pygame
+from pygame import mixer
 
 #initialize the pygame
 pygame.init()
+mixer.init()
 
-#create the screen
+#create the screen and set volume
 screen = pygame.display.set_mode((800, 600))
+mixer.music.set_volume(0.8)
 
-#title and icon
+#set title and icon (for window)
 pygame.display.set_caption("The Call of Cthulhu - Survival Horror Game")
-icon = pygame.image.load('pictures/icon.png')
+icon = pygame.image.load("pictures/icon.png")
 pygame.display.set_icon(icon)
 
-#background image and title
-background = pygame.image.load("pictures/startBG.png")
+#load audio and pictures
+startBG = pygame.image.load("pictures/startBG.png")
 title = pygame.image.load("pictures/mainTitle.png")
 startButton = pygame.image.load("pictures/startButton.png")
+opening = mixer.music.load("audio/BeforeDawn.wav")
 
-#Game Loop
+#play background music
+mixer.music.play(-1)
+
+###############  MAIN GAME LOOP  ################
 running = True
 while running:
 
@@ -33,8 +40,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    #beginnging game display
-    screen.blit(background, (0,70))
+    #start game display
+    screen.blit(startBG, (0,70))
     screen.blit(title, (40, 50))
     screen.blit(startButton, (350, 470))
+
     pygame.display.update()
+
+################  EXIT GAME LOOP ################
+
+#stop background music
+mixer.music.stop()
